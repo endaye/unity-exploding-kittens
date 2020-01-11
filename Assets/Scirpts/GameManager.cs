308f752951888c681ac6ca83a371accda733f7a7
Yuancheng Zhang <en@endaye.com>
2020-01-12 02:23:37 +0800

    update scene

---
+++ Assets/Scirpts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public enum GameState {
+        None = 0,
+        Wait = 1,
+        Deal = 2,
+        Start = 3,
+        Finish = 4,
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        InitGame();
+    }
+
+    void InitGame() {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
